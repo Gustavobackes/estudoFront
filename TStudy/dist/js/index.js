00084d0391bcc162ffe3f8e9280be2e98aec37e5
gustavobackes <gustavo.kitzberger@projuris.com.br>
2022-09-13 14:47:14 -0300

    o que foi excluido ta no notion

---
+++ TStudy/dist/js/index.js
@@ -0,0 +1,8 @@
+"use strict";
+const listaDeNomes = ["gustavo", "matheus", "nomeexemplo"];
+const firstname = "gustavo";
+const numero = 3;
+const listaDeNumeros = [1, 2, 3, 4];
+//tuplas
+let myTuple = [2, "valor", ["valor1", "valor2"]];
+console.log(myTuple);
---
+++ TStudy/src/index.ts
@@ -0,0 +1,8 @@
+const listaDeNomes: string[] = ["gustavo", "matheus", "nomeexemplo"];
+const firstname: string = "gustavo";
+const numero: number = 3;
+const listaDeNumeros: number[] = [1, 2, 3, 4];
+
+//tuplas
+let myTuple: [number, string, string[]] = [2, "valor", ["valor1", "valor2"]];
+console.log(myTuple);
