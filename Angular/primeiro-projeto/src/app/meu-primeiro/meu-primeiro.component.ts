b68b4627fa16d2fe3f8597634caf56f613b3d5e8
gustavobackes <gustavo.kitzberger@projuris.com.br>
2022-10-13 16:32:09 -0300

    comeco do projeto

---
+++ Angular/primeiro-projeto/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'meu-primeiro-component',
+  template: ` <p>teste de component</p> `,
+})
+export class MeuPrimeiroComponent {}
